8a7e6c9c1cceb6dc558a4da6dbc20f5d1ad9a454
SoubirSaian <soubir2018@gmail.com>
2022-02-21 22:47:56 +0600

    side-nav

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import SideNav from '../SideNav/SideNav';
+import Main from '../Main/Main';
 import './Home.css';
 
 const Home = () => {
     return (
-        <div>
+        <div className='home-page'>
             <SideNav></SideNav>
+            <Main></Main>
         </div>
     );
 };
---
+++ src/components/Main/Main.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Main.css';
+
+const Main = () => {
+    return (
+        <div className='main-part'>
+            <div className='title-bar'>
+                <h6>Section</h6>
+                <span>0.2$XYZ</span>
+            </div>
+            <div className='main-detail'>
+                <p>Lorium Ipsum is simply dummy text of the printing and typesetting industry</p>
+                <button>Tutorial</button>
+            </div>
+        </div>
+    );
+};
+
+export default Main;
